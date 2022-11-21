32899f51814cf3a878e54ede333ace5cf3c01f2d
Augusto Citton Ludwig <augusto.citton@dbccompany.com.br>
2022-11-21 12:03:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import { useState } from 'react'
+import Title from './components/Title';
 
 function App() {
+
+  const n = 15;
+  const [name] = useState('Augusto');
+
+  const redTitle = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* CSS GLOBAL */}
+      <h1>React com CSS</h1>
+      {/* CSS DE COMPONENTE */}
+      <MyComponent />
+      <p>Esse parágrafo é do App.js</p>
+      {/* INLINE CSS */}
+      <p style={{color: 'blue', padding: '25px', borderTop: '2px solid red'}}>
+        Esse elemento foi estilizado de forma inline
+      </p>
+      <p style={{color: 'magenta', padding: '25px', borderTop: '2px solid red'}}>
+        Esse elemento foi estilizado de forma inline
+      </p>
+      {/* CSS INLINE DINÂMICO */}
+      <h2 style={n < 10 ? ({color: 'purple'}) : ({color: 'pink'})}>
+      CSS dinâmico
+      </h2>
+      <h2 style={n > 10 ? ({color: 'purple'}) : ({color: 'pink'})}>
+      CSS dinâmico
+      </h2>
+      <h2 style={name === 'Augusto' ? ({color: 'green', backgroundColor: '#000'}) : (null)}>
+      Teste nome
+      </h2>
+      {/* CLASSE DINÂMICA */}
+      <h2 className= {redTitle ? 'red-title' : 'title'}>
+        Este título vai ter classe dinâmica
+      </h2>
+      {/* CSS MODULES */}
+      <Title />
     </div>
   );
 }
